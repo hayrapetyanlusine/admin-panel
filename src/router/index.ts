43b720a776ab67dff8548e08edcc500419fcddb7
hayrapetyanlusine <lusinehayrapetyan0220@gmail.com>
2025-08-18 21:28:37 +0400

    feat: set users page as default child route for dashboard

--- src/router/index.ts
+++ src/router/index.ts
@@ -22,6 +22,7 @@ const router = createRouter({
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/Dashboard.vue'),
       meta: { requiresAuth: true },
+      redirect: '/dashboard/users',
       children: [
         {
           path: 'users',
