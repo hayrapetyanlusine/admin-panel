e55cb2a8eb70f2442c60c72adeac406ed725398e
hayrapetyanlusine <lusinehayrapetyan0220@gmail.com>
2025-08-19 11:39:16 +0400

    feat: back to dashboard link

--- src/views/UserDetail.vue
+++ src/views/UserDetail.vue
@@ -6,9 +6,7 @@ import type { User } from '@/interfaces/user.ts';
 const route = useRoute()
 const userId = route.params.id
 
-const { data: user, error } = useFetch<User>(
-    `https://jsonplaceholder.typicode.com/users/${userId}`
-)
+const { data: user, error } = useFetch<User>(`https://jsonplaceholder.typicode.com/users/${userId}`)
 </script>
 
 <template>
@@ -16,16 +14,45 @@ const { data: user, error } = useFetch<User>(
   <p v-else-if="error">Failed to load data: {{ error.message }}</p>
 
   <div v-if="user" class="content">
-    <h4>{{ user.name }}</h4>
-    <p>Email: {{ user.email }}</p>
-    <p>Phone: {{ user.phone }}</p>
-    <p>Website: {{ user.website }}</p>
+    <RouterLink to="/dashboard" class="back">
+      <i class="pi pi-home"></i>
+      Back to Dashboard
+    </RouterLink>
+
+    <div class="content-info">
+      <h4 class="user-name">{{ user.name }}</h4>
+      <p>Email: {{ user.email }}</p>
+      <p>Phone: {{ user.phone }}</p>
+      <p>Website: {{ user.website }}</p>
+    </div>
   </div>
 </template>
 
 <style scoped>
 .content {
   color: black;
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+}
+
+.content-info {
+  display: flex;
+  flex-direction: column;
+  gap: 5px;
+  font-size: 18px;
+}
+
+.user-name {
+  font-weight: bold;
   font-size: 20px;
+}
+
+.back {
+  font-size: 15px;
+
+  &:hover {
+    color: black;
+  }
 }
 </style>
